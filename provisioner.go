1d47c01967b9f2bf9b0371c0fff367902b1d4914
pragnesh <pg.radadia@gmail.com>
2020-02-03 16:58:04 +0530

    regex fix for fabric3

--- provisioner.go
+++ provisioner.go
@@ -142,7 +142,7 @@ func (p *Provisioner) getVersion() error {
 		return err
 	}
 
-	versionRe := regexp.MustCompile(`Fabric (\d+\.\d+[.\d+]*)`)
+	versionRe := regexp.MustCompile(`Fabric3 (\d+\.\d+[.\d+]*)`)
 	matches := versionRe.FindStringSubmatch(string(out))
 	if matches == nil {
 		return fmt.Errorf(
